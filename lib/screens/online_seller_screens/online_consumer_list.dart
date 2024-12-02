7aff909179379de5f3a73a48b3f9d2721f190d60
yuna <younasi0318@cau.ac.kr>
2024-12-03 01:47:52 +0900

    소비자 주문 목록 수정

--- lib/screens/online_seller_screens/online_consumer_list.dart
+++ lib/screens/online_seller_screens/online_consumer_list.dart
@@ -11,6 +11,7 @@ class OnlineConsumerList extends StatefulWidget {
 
 class _OnlineConsumerListState extends State<OnlineConsumerList> {
   String? festivalName;
+  Map<String, List<dynamic>>? cachedConsumerData; // 데이터를 캐싱할 변수
 
   @override
   void didChangeDependencies() {
@@ -18,43 +19,11 @@ class _OnlineConsumerListState extends State<OnlineConsumerList> {
     festivalName = ModalRoute.of(context)?.settings.arguments as String?;
   }
 
-  Future<Map<String, int>> _calculateTotalOrders() async {
-    final uid = FirebaseAuth.instance.currentUser?.uid;
-    if (uid == null || festivalName == null) return {};
-
-    final docRef = FirebaseFirestore.instance
-        .collection('Users')
-        .doc(uid)
-        .collection('online_consumer_list')
-        .doc(festivalName);
-
-    final docSnapshot = await docRef.get();
-    if (!docSnapshot.exists) return {};
-
-    final data = docSnapshot.data();
-    if (data == null) return {};
-
-    final Map<String, int> totalOrders = {};
-
-    for (var field in data.values) {
-      if (field is List<dynamic>) {
-        for (var i = 1; i < field.length; i++) {
-          final orderItem = field[i];
-          if (orderItem is Map<String, dynamic>) {
-            final String itemName = orderItem['itemName'] ?? '';
-            final int quantity = orderItem['quantity'] ?? 0;
-
-            if (itemName.isNotEmpty) {
-              totalOrders[itemName] = (totalOrders[itemName] ?? 0) + quantity;
-            }
-          }
-        }
-      }
-    }
-    return totalOrders;
-  }
-
   Future<Map<String, List<dynamic>>> _fetchConsumerData() async {
+    if (cachedConsumerData != null) {
+      return cachedConsumerData!; // 이미 데이터를 가져왔으면 캐시된 데이터 반환
+    }
+
     final uid = FirebaseAuth.instance.currentUser?.uid;
     if (uid == null || festivalName == null) return {};
 
@@ -78,6 +47,8 @@ class _OnlineConsumerListState extends State<OnlineConsumerList> {
         print('Invalid data format for key ${entry.key}');
       }
     }
+
+    cachedConsumerData = result; // 데이터 캐싱
     return result;
   }
 
@@ -92,65 +63,11 @@ class _OnlineConsumerListState extends State<OnlineConsumerList> {
     }
   }
 
-  void _showTotalOrdersPopup() async {
-    final totalOrders = await _calculateTotalOrders();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      '총 주문 물품',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 16),
-                    SizedBox(
-                      width: double.maxFinite,
-                      height: 300,
-                      child: ListView.builder(
-                        itemCount: totalOrders.length,
-                        itemBuilder: (context, index) {
-                          final itemName = totalOrders.keys.elementAt(index);
-                          final quantity = totalOrders[itemName]!;
-                          return ListTile(
-                            title: Text('$itemName : $quantity개'),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 8,
-                right: 8,
-                child: IconButton(
-                  icon: const Icon(Icons.close, color: Colors.black),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('소비자 주문 목록'),
-        backgroundColor: const Color(0xFFFDBE85),
         centerTitle: true,
       ),
       body: FutureBuilder<Map<String, List<dynamic>>>(
@@ -167,104 +84,146 @@ class _OnlineConsumerListState extends State<OnlineConsumerList> {
           }
 
           final consumerData = snapshot.data!;
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFFDBE85),
-                  ),
-                  onPressed: _showTotalOrdersPopup,
-                  child: const Text('총 주문 물품'),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: ListView.builder(
-                    itemCount: consumerData.length,
-                    itemBuilder: (context, index) {
-                      final consumerId = consumerData.keys.elementAt(index);
-                      final List<dynamic> orders = consumerData[consumerId]!;
-                      final Map<String, dynamic> consumerInfo = orders.first as Map<String, dynamic>;
-                      final String name = consumerInfo['name'] ?? '';
-                      final String phone = _formatPhone(consumerInfo['phone'] ?? '');
-                      final String zipcode = consumerInfo['zipcode'] ?? '';
-                      final String address = consumerInfo['address'] ?? '';
+          return ListView.builder(
+            itemCount: consumerData.length,
+            itemBuilder: (context, index) {
+              final consumerId = consumerData.keys.elementAt(index);
+              final List<dynamic> orders = consumerData[consumerId]!;
+              final Map<String, dynamic> consumerInfo = orders.first as Map<String, dynamic>;
+              final String name = consumerInfo['name'] ?? '';
+              final String phone = _formatPhone(consumerInfo['phone'] ?? '');
+              final String zipcode = consumerInfo['zipcode'] ?? '';
+              final String address = consumerInfo['address'] ?? '';
 
-                      return Card(
-                        color: Colors.white,
-                        margin: const EdgeInsets.symmetric(vertical: 8.0),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                child: ExpansionTile(
+                  title: Text(
+                    '주문자 정보',
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                  subtitle:
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('$name'),
+                      const SizedBox(
+                        height: 15,
+                        child: VerticalDivider(
+                          thickness: 1,
+                          color: Colors.grey,
+                          width: 20,
+                        ),
+                      ),
+                      Text('$phone'),
+                    ],
+                  ),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
                             children: [
-                              Expanded(
-                                flex: 1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      '성함: $name',
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 14,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 4),
-                                    Text('연락처: $phone'),
-                                    const SizedBox(height: 4),
-                                    Text('우편번호: $zipcode'),
-                                    const SizedBox(height: 4),
-                                    Text('주소: $address'),
-                                  ],
+                              Container(
+                                width: 70,
+                                child: const Text(
+                                  '주문자',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
+                              Text('$name'),
+                            ],
+                          ),
+                          const SizedBox(height: 4),
+                          Row(
+                            children: [
                               Container(
-                                width: 3,
-                                margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      Colors.grey[400]!,
-                                      Colors.grey[300]!,
-                                      Colors.grey[200]!,
-                                      Colors.transparent
-                                    ],
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
+                                width: 70,
+                                child: const Text(
+                                  '주소',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w600,
                                   ),
                                 ),
                               ),
                               Expanded(
-                                flex: 1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    for (var i = 1; i < orders.length; i++)
-                                      if (orders[i] is Map<String, dynamic>)
-                                        Text(
-                                          '${orders[i]['itemName'] ?? ''} : ${orders[i]['quantity'] ?? 0}개',
-                                        ),
-                                  ],
+                                child: Text(
+                                  '$address',
+                                  softWrap: true,
+                                  maxLines: 5,
+                                  overflow: TextOverflow.ellipsis,
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      );
-                    },
-                  ),
+                          const SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Container(
+                                width: 70,
+                                child: const Text(
+                                  '연락처',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Text('$phone'),
+                            ],
+                          ),
+                          const SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Container(
+                                width: 70,
+                                child: const Text(
+                                  '우편번호',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Text('$zipcode'),
+                            ],
+                          ),
+                          const SizedBox(height: 4),
+                          const Divider(),
+                          const SizedBox(height: 4),
+                          const Text(
+                            '구매 정보',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          for (var i = 1; i < orders.length; i++)
+                            if (orders[i] is Map<String, dynamic>)
+                              Column(
+                                children: [
+                                  Text(
+                                    '${orders[i]['itemName'] ?? ''} : ${orders[i]['quantity'] ?? 0}개',
+                                  ),
+                                  const SizedBox(height: 4),
+                                ],
+                              ),
+                          const SizedBox(height: 16),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+              );
+            },
           );
         },
       ),
